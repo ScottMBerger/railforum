ae71acb195acc6702276a59d9bbc21ba73838ef0
Scott Berger <berger17@email.franklin.edu>
2016-01-31 19:40:41 +0000

    Finish user signup testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome, " + @user.username + ". Your account is setup!"
+      redirect_to @user
     else
       render 'new'
     end
